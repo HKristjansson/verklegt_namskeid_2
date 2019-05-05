4d1dcbdb98b4032d76c6b22f0b0e06030f2631ee
ermir18 <ermir18@ru.is>
2019-05-05 20:35:55 +0000

    added small logic

--- user/models.py
+++ user/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
 class User(models.model):
-    pass
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    b_day = models.DateField
+    search_history = models.CharField(max_length=255)
+    is_employee = models.BooleanField(bool=False)
