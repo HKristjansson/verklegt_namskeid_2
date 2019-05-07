3859426d065ee82a23228a031edc15ac5691fa00
Hannes Kristjánsson <hannes.kristjansson@gmail.com>
2019-05-07 19:52:54 +0000

    Merge branch 'master' of https://github.com/ermir18/verklegt_namskeid_2
    Signed-off-by: Hannes Kristjánsson <hannes.kristjansson@gmail.com>
    
    # Conflicts:
    #	.idea/misc.xml
    #	.idea/workspace.xml

--- user/forms/registration_form.py
+++ user/forms/registration_form.py
@@ -41,4 +41,4 @@ class UserUpdateForm(forms.ModelForm):
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['profile_image']
+        fields = ['image']
