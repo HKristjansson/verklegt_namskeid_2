6a685c5cc601412f5e78deda64dd8459d0329ad2
Hannes Kristjánsson <hannes.kristjansson@gmail.com>
2019-05-09 19:30:23 +0000

    ADD REALTOR NOW ADDED (and works)

---
+++ seller/forms/seller_form.py
@@ -0,0 +1,30 @@
+from django.forms import ModelForm, widgets
+from django import forms
+from seller.models import Seller
+
+
+class ApartmentUpdateForm(ModelForm):
+    class Meta:
+        model = Seller
+        exclude = ['id']
+        widgets = {
+            'name': widgets.TextInput(attrs={'class': 'form-control'}),
+            'image': widgets.NumberInput(attrs={'class': 'form-control'}),
+            'year_of_start': widgets.TextInput(attrs={'class': 'form-control'}),
+            'available': widgets.CheckboxInput(attrs={'class': 'checkbox'})
+
+        }
+
+
+class SellerAddForm(ModelForm):
+    image = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = Seller
+        exclude = ['id']
+        widgets = {
+            'name': widgets.TextInput(attrs={'class': 'form-control'}),
+            'image': widgets.NumberInput(attrs={'class': 'form-control'}),
+            'year_of_start': widgets.TextInput(attrs={'class': 'form-control'}),
+            'available': widgets.CheckboxInput(attrs={'class': 'checkbox'})
+        }
