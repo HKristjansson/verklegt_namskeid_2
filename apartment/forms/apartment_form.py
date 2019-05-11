17468c07c760856cc5079ec35bed00e3aacca2e0
Hannes Kristjánsson <hannes.kristjansson@gmail.com>
2019-05-11 22:19:54 +0000

    buy apartmen uneditable cons: sold needs to be set to true, and buyer_id doesn't seem to be right, (if I buy, it gets no. 3, but my profile is no 11).

--- apartment/forms/apartment_form.py
+++ apartment/forms/apartment_form.py
@@ -8,21 +8,37 @@ class ApartmentBuyForm(forms.ModelForm):
         exclude = ['id', 'created', 'updated']
         readonly_fields = ['address', 'number', 'zip', 'description', 'rooms', 'size', 'price', 'category',
                            'seller', 'owner_name', 'owner_ssn', 'owner_phone']
+
         widgets = {
-            #'address': widgets.TextInput(attrs={'class': 'form-control'}),
-            'number': widgets.NumberInput(attrs={'class': 'form-control'}),
-            'zip': widgets.NumberInput(attrs={'class': 'form-control'}),
-            'description': widgets.TextInput(attrs={'class': 'form-control'}),
-            'rooms': widgets.NumberInput(attrs={'class': 'form-control'}),
-            'size': widgets.NumberInput(attrs={'class': 'form-control'}),
-            'price': widgets.NumberInput(attrs={'class': 'form-control'}),
-            'category': widgets.Select(attrs={'class': 'form-control'}),
-            'seller': widgets.Select(attrs={'class': 'form-control'}),
-            'owner_name': widgets.TextInput(attrs={'class': 'form-control'}),
-            'owner_ssn': widgets.NumberInput(attrs={'class': 'form-control'}),
-            'owner_phone': widgets.NumberInput(attrs={'class': 'form-control'}),
-            'sold': widgets.CheckboxInput(attrs={'class': 'checkbox'}),
-            'buyer_id': widgets.NumberInput(attrs={'class': 'form-control'})
+            'address': widgets.TextInput(attrs={'readonly': True}),
+            'number': widgets.TextInput(attrs={'readonly': True}),
+            'zip': widgets.TextInput(attrs={'readonly': True}),
+            'description': widgets.TextInput(attrs={'readonly': True}),
+            'rooms': widgets.TextInput(attrs={'readonly': True}),
+            'size': widgets.TextInput(attrs={'readonly': True}),
+            'price': widgets.TextInput(attrs={'readonly': True}),
+            'category': widgets.TextInput(attrs={'readonly': True}),
+            'seller': widgets.TextInput(attrs={'readonly': True}),
+            'owner_name': widgets.TextInput(attrs={'readonly': True}),
+            'owner_ssn': widgets.TextInput(attrs={'readonly': True}),
+            'owner_phone': widgets.TextInput(attrs={'readonly': True})
+
+
+
+
+            # 'number': widgets.NumberInput(attrs={'class': 'form-control'}),
+            # 'zip': widgets.NumberInput(attrs={'class': 'form-control'}),
+            # 'description': widgets.TextInput(attrs={'class': 'form-control'}),
+            # 'rooms': widgets.NumberInput(attrs={'class': 'form-control'}),
+            # 'size': widgets.NumberInput(attrs={'class': 'form-control'}),
+            # 'price': widgets.NumberInput(attrs={'class': 'form-control'}),
+            # 'category': widgets.Select(attrs={'class': 'form-control'}),
+            # 'seller': widgets.Select(attrs={'class': 'form-control'}),
+            # 'owner_name': widgets.TextInput(attrs={'class': 'form-control'}),
+            # 'owner_ssn': widgets.NumberInput(attrs={'class': 'form-control'}),
+            # 'owner_phone': widgets.NumberInput(attrs={'class': 'form-control'}),
+            # 'sold': widgets.CheckboxInput(attrs={'class': 'checkbox'}),
+            # 'buyer_id': widgets.NumberInput(attrs={'class': 'form-control'})
         }
 
 class ApartmentUpdateForm(ModelForm):
