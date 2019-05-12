51c97dc105b9a76e7c346e5e21bcfe8f8d64c666
ermir18 <ermir18@ru.is>
2019-05-12 21:32:22 +0000

    added mb

--- apartment/views.py
+++ apartment/views.py
@@ -66,11 +66,9 @@ def update_apartment(request, id):
     instance = get_object_or_404(Apartment, pk=id)
     if request.method == 'POST':
         form = ApartmentUpdateForm(data=request.POST, instance=instance)
-        # apartment_image = ApartmentImage(image=request.POST['image'], apartment=instance)
         if form.is_valid():
-            edit = form.save(commit=False)
-            edit.save()
-            # apartment_image.save()
+            ApartmentImage.objects.filter(pk=id).update(image=request.POST['image'])
+            form.save()
             return redirect('apartment_details', id=id)
     else:
         form = ApartmentUpdateForm(instance=instance)
