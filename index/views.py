706da48884526e4f88d1c40278575d5ed7107a20
ermir18 <ermir18@ru.is>
2019-05-13 22:45:39 +0000

    added style font size

--- index/views.py
+++ index/views.py
@@ -8,21 +8,21 @@ from django.views.generic import ListView
 
 class CardListView(ListView):
     model = Apartment
-    template_name = 'index/index.html'  # <app>/<model>_<viewtype>.html
+    template_name = 'index/index.html'
     context_object_name = 'apartments'
     ordering = ['-created']
     paginate_by = 8
 
 
-def index(request):
-    apartment_list = Apartment.objects.all().order_by('-created')
-    building_types = ApartmentCategory.objects.all()
-    zip_code = ZIP.objects.all()
-    paginator = Paginator(apartment_list, 4)
-    page = request.POST.get('page', 'Default')
-    apartments = paginator.get_page(page)
-    context = {'apartments': apartments, 'building_types': building_types, 'zip': zip_code}
-    return render(request, 'index/index.html', context)
+# def index(request):
+#     apartment_list = Apartment.objects.all().order_by('-created')
+#     building_types = ApartmentCategory.objects.all()
+#     zip_code = ZIP.objects.all()
+#     paginator = Paginator(apartment_list, 4)
+#     page = request.POST.get('page', 'Default')
+#     apartments = paginator.get_page(page)
+#     context = {'apartments': apartments, 'building_types': building_types, 'zip': zip_code}
+#     return render(request, 'index/index.html', context)
 
 
 def about(request):
