80dcc43ccb432ba23b978f6b61ae60704ad485d9
Hannes Kristjánsson <hannes.kristjansson@gmail.com>
2019-05-14 16:05:04 +0000

    buying process, step 2 and 3 (confirmation step, and "congratulation step") Not completed, but... getting there.

--- apartment/urls.py
+++ apartment/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     path('update_apartment/<int:id>', views.update_apartment, name="update_apartment"),
     path('search_apartment/', views.search_apartment, name="search_apartment"),
     path('buy_apartment_step_one/<int:id>', views.buy_apartment_step_one, name='buy_apartment_step_one'),
+    path('buy_apartment_step_two/<int:id>', views.buy_apartment_step_two, name='buy_apartment_step_two'),
+    path('buy_apartment_step_three/<int:id>', views.buy_apartment_step_three, name='buy_apartment_step_three'),
     path('sold_apartments/', views.sold_apartments, name="sold_apartments"),
 ]
